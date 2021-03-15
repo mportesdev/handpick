486e77bf014b130641376766328691c65f4eb87b
mportesdev <michalportes1@gmail.com>
2021-03-15 12:41:52 +0100

    Add function doctring and include it in README

--- README.rst
+++ README.rst
@@ -55,3 +55,28 @@ Example 3: handling dictionary keys
 
     ['_Py']
     ['_key', '_str', '_Py', '_n']
+
+API reference
+-------------
+
+handpick.pick(root, predicate, dict_keys=False, strings=False, bytes_like=False)
+    Yield all objects recursively from ``root`` for which
+    ``predicate(obj)`` is true.
+
+    ``root`` should be an iterable container. ``predicate`` should be a
+    callable taking one object as argument and returning a Boolean
+    value. If ``predicate`` is not callable, equality will be used as the
+    picking criteria, i.e. objects for which ``obj == predicate`` is true
+    will be yielded.
+
+    When traversing a mapping, only its values are inspected by
+    default. If ``dict_keys`` is set to True, both keys and values of the
+    mapping are inspected.
+
+    By default, strings are not considered containers and therefore not
+    visited by the recursive algorithm. This can be changed by setting
+    ``strings`` to True. Strings of length 0 or 1 are never visited.
+
+    By default, bytes-like sequences (bytes and bytearrays) are not
+    considered containers and therefore not visited by the recursive
+    algorithm. This can be changed by setting ``bytes_like`` to True.
--- handpick/core.py
+++ handpick/core.py
@@ -7,6 +7,27 @@ from collections.abc import Mapping, Iterable
 
 
 def pick(root, predicate, dict_keys=False, strings=False, bytes_like=False):
+    """Yield all objects recursively from `root` for which
+    `predicate(obj)` is true.
+
+    `root` should be an iterable container. `predicate` should be a
+    callable taking one object as argument and returning a Boolean
+    value. If `predicate` is not callable, equality will be used as the
+    picking criteria, i.e. objects for which `obj == predicate` is true
+    will be yielded.
+
+    When traversing a mapping, only its values are inspected by
+    default. If `dict_keys` is set to True, both keys and values of the
+    mapping are inspected.
+
+    By default, strings are not considered containers and therefore not
+    visited by the recursive algorithm. This can be changed by setting
+    `strings` to True. Strings of length 0 or 1 are never visited.
+
+    By default, bytes-like sequences (bytes and bytearrays) are not
+    considered containers and therefore not visited by the recursive
+    algorithm. This can be changed by setting `bytes_like` to True.
+    """
     if not isinstance(root, Iterable):
         return
     if isinstance(root, str) and (not strings or len(root) <= 1):
