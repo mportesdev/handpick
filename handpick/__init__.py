bcd77ac7a8062c3319c8caaf39f1f8d3133c8682
Michal Porteš <michalportes1@gmail.com>
2021-04-01 17:16:15 +0200

    Bump version to 0.4.0

--- handpick/__init__.py
+++ handpick/__init__.py
@@ -1,5 +1,5 @@
 from .core import pick, predicate, NO_CONTAINERS, NO_LIST_DICT
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 __all__ = ['__version__', 'pick', 'predicate', 'NO_CONTAINERS', 'NO_LIST_DICT']
