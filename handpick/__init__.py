04e006bef2a1cb811c869953bef59162a3af7dd2
Michal Porteš <michalportes1@gmail.com>
2021-04-09 19:26:40 +0200

    Bump version to 0.6.0

--- handpick/__init__.py
+++ handpick/__init__.py
@@ -5,7 +5,7 @@ find all objects that meet certain criteria.
 
 from .core import pick, flat, predicate, ALL, NO_CONTAINERS, NO_LIST_DICT
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 
 __all__ = [
     '__version__', 'pick', 'flat', 'predicate',
