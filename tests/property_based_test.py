6de50be314639fe461048a0a2a799f50afa205d9
Michal Porteš <michalportes1@gmail.com>
2022-06-22 14:49:33 +0200

    Enable decoration by Predicate instance

--- tests/property_based_test.py
+++ tests/property_based_test.py
@@ -3,7 +3,9 @@ import string
 from hypothesis import given
 import hypothesis.strategies as st
 
-from handpick import pick, IS_COLLECTION, is_type, not_type
+from handpick import pick, Predicate, IS_COLLECTION, is_type, not_type
+
+from tests import is_even
 
 strings = st.text(string.printable)
 keys = strings
@@ -34,3 +36,10 @@ def test_is_type_not_type(type_, value):
     pred_2 = not_type(type_)
     assert pred_1(value) is (~pred_2)(value)
     assert (~pred_1)(value) is pred_2(value)
+
+
+@given(values)
+def test_predicate_decorator_call(value):
+    pred_1 = Predicate(is_even)
+    pred_2 = Predicate()(is_even)
+    assert pred_1(value) is pred_2(value)
